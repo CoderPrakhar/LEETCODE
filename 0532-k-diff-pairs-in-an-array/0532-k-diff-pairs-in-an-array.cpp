cd97ef62513da1c9c00bf47e7fedc69be9784c4b
Prakhar Srivastava <prakhar830380@gmail.com>
2026-08-08 22:44:23 +0530

    Time: 2 ms (78.79%), Space: 15 MB (84.32%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-	int findPairs(vector<int>& nums, int k) {
-		sort(nums.begin(),nums.end());
-		int fast=1;
-		int slow=0;
-		int ans=0;
-		while(slow<nums.size() && fast<nums.size()){
-			if(nums[fast]-nums[slow]==k){
-				ans++;
-				fast++;
-				slow++;
-				while(fast<nums.size() && nums[fast]==nums[fast-1]){
-					fast++;
-				}
-			}
-			else if(nums[fast]-nums[slow]>k){
-				slow++;  
-				if(fast-slow==0){
-					fast++;}
-			}
-			else{      
-				fast++;
-			}         
-		}
-		return ans;
+    int findPairs(vector<int>& nums, int k) {
 
-	}
+        sort(nums.begin(), nums.end());
+
+        int i = 0;
+        int j = 1;
+        int count = 0;
+
+        while (j < nums.size()) {
+            if (i == j) {
+                j++;
+                continue;
+            }
+
+            int diff = nums[j] - nums[i];
+            if (diff < k) {
+                j++;
+            }
+            else if (diff > k) {
+                i++;
+            }
+            else {
+                count++;
+
+                //avoid dupicates
+                int left = nums[i];
+                int right = nums[j];
+
+                while (i < nums.size() && nums[i] == left) {
+                    i++;
+                }
+
+                while (j < nums.size() && nums[j] == right) {
+                    j++;
+                }
+            }
+        }
+
+        return count;
+    }
 };
