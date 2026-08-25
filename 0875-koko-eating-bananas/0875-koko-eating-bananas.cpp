e3aabbde86b6bfc3e646fbb3f691ce906d01b646
Prakhar Srivastava <prakhar830380@gmail.com>
2026-08-25 20:54:13 +0530

    Time: 10 ms (39.57%), Space: 22.9 MB (88.74%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    int findMax(vector<int> &v){
-        int maxi=INT_MIN;
-        int n=v.size();
+    bool isPossible(vector<int>& piles,int mid, int h){
+        int hr=0;
+        for(int i=0; i<piles.size(); i++){
+            if(piles[i]<=mid){
+            hr++;
+            }
+        else if(piles[i]%mid == 0){
+            hr+= piles[i]/mid;
+            }
+        else if(piles[i]%mid != 0){
+            hr+= (piles[i]/mid)+1;
+            }
 
-        for(int i=0; i<n; i++){
-            maxi=max(maxi, v[i]);
+        if(hr>h){
+            return false;
+            }
         }
-        return maxi;
+        return true;
     }
-
-    long long calculateTotalH(vector<int>& v, int hr){
-        long long totalH=0;
-        int n=v.size();
-
-        for(int i=0; i<n; i++){
-            totalH+=ceil((double)v[i]/hr);
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int st=1;
+        int maxi= INT_MIN;
+        int ans;
+        for(int i=0; i<piles.size(); i++){
+            maxi = max(maxi, piles[i]);
         }
+        int end= maxi;
 
-        return totalH;
-    }
-
-    int minEatingSpeed(vector<int>& v, int h) {
-        int st=1, end=findMax(v);
         while(st<=end){
             int mid=(st+end)/2;
-            long long totalH=calculateTotalH(v, mid);
-
-            if(totalH<=h){
+            if(isPossible(piles,mid,h)){
+                ans=mid;
                 end=mid-1;
-            }else{
+            }
+            else{
                 st=mid+1;
             }
         }
-
-        return st;
+    return ans;
     }
 };
