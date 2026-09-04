7fe1a5933c4555db779de4f624c5cc588855cc26
Prakhar Srivastava <prakhar830380@gmail.com>
2026-09-04 20:21:16 +0530

    Time: 46 ms (11.33%), Space: 45.2 MB (9.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    set<vector<int>> s;
-
-    void getAllCombinations(vector<int>& arr, int idx, int tar,
-                            vector<vector<int>>& ans,
-                            vector<int>& combin) {
-
-        if (tar == 0) {
-            if (s.find(combin) == s.end()) {
-                ans.push_back(combin);
-                s.insert(combin);
+    void findCombination(int i , int target , vector<int> &arr,  
+        vector<vector<int>> &ans,
+        vector<int> ds){
+            if(i==arr.size() ){
+                if(target == 0){
+                    ans.push_back(ds);
+                }
+                return ;
             }
-            return;
+
+            //pick up the element
+            if(arr[i]<= target ){
+                ds.push_back(arr[i]);
+
+                findCombination(i, target-arr[i], arr, ans, ds);
+                ds.pop_back();
+            }
+
+            //not picking the element
+            findCombination(i+1, target, arr, ans, ds);
         }
 
-        if (idx == arr.size() || tar < 0) {
-            return;
-        }
-
-        combin.push_back(arr[idx]);
-
-        // Single inclusion
-        getAllCombinations(arr, idx + 1, tar - arr[idx], ans, combin);
-
-        // Multiple inclusion
-        getAllCombinations(arr, idx, tar - arr[idx], ans, combin);
-
-        combin.pop_back();
-
-        //exclusion
-                getAllCombinations(arr, idx + 1, tar , ans, combin);
-
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> combin;
+        vector<int> ds;
 
-        getAllCombinations(arr, 0, target, ans, combin);
+        findCombination(0, target, candidates, ans, ds);
 
         return ans;
     }
