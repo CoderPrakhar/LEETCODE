6cf814f1ef09de6d46ca7961689fe9b89ccc8b13
Prakhar Srivastava <prakhar830380@gmail.com>
2026-09-10 20:19:01 +0530

    Time: 0 ms (100%), Space: 10.5 MB (71.01%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    void func(vector<vector<int>> &ans, vector<int>& ds, vector<int>& nums, int map[]){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
-            return;
+    void func(int index, vector<int>& nums , vector<vector<int>> & ans ){
+        if(index== nums.size()){
+            ans.push_back(nums);
+            return ;
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(!map[i]){
-                ds.push_back(nums[i]);
-                map[i] = 1;
-
-                func(ans, ds, nums, map);
-
-                map[i]=0;
-                ds.pop_back();
-            }
+        for(int i=index; i<nums.size() ;  i++){ 
+            swap(nums[index] , nums[i]);
+            func(index+1, nums, ans);
+            swap(nums[index], nums[i]); // backtracking...       
         }
     }
-
-    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int map[nums.size()] ;
-        for(int i=0; i<nums.size(); i++) {
-            map[i] = 0;
-        }
-        func(ans,ds, nums, map);
+        func(0,nums , ans);
 
         return ans;
     }
